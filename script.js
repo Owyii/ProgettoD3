c4cb1ad8000fbaa0ac7541470bc4a2c5ed7cee88
Emanuele Ostanello <e.ostanello@gmail.com>
2023-02-18 14:20:42 +0100

    Now is all function and fixed second graph

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
+//Global variables
 const svgEl = document.getElementById('chart')
 const width = svgEl.getAttribute('width')
 const height = svgEl.getAttribute('height')
 const padding = 60
 const svg = d3.select('#chart')
 const info = d3.select('#info')
-
 
 const svg_2 = d3.select('#chart_due')
 const svgEl_2 = document.getElementById('chart_due')
@@ -15,9 +14,8 @@ const height_2 = svgEl.getAttribute('height')
 let pointsColor1 = '#87CEFA' // the colour of one of the Countries
 let pointsColor2 = '#90EE90' // the colour of one of the Countries
 let textColor = '#194d30'
-
 var current = ""
-
+var isShowing = false
 
 // in dataset.js you see that the data is in comma separated values format. 
 // this is a way to decode it by also expliciting the types, so that the d3 dataset is correctly made
@@ -33,14 +31,12 @@ const data = d3.csvParse(dataset, d => {
 	}
 });
 
-
-
 // sorting the dataset by date
 data.sort((a, b) => d3.ascending(a.date, b.date))
 
 //take day by day array and return grouped by week
 var getArrByWeek = function(data){
-	var first_day = 1577833200000
+	const first_day = 1577833200000
 	console.log(first_day)
 
 	return d3.rollups(
@@ -57,48 +53,15 @@ var getArrByWeek = function(data){
 				return new Date(d.date.getFullYear(),d.date.getMonth(),d.date.getDate() - diff)
 			}
 		},
-		//d => d.date
 	)
 	.map(([d,c]) => ({Day: d,Cases: c}))
 }
 
 var groupdata = getArrByWeek(data)
 
-
-console.log(groupdata)
-
-// X asix
-var Theight = height - padding
-var xAxis = d3.scaleTime()
-	.domain(d3.extent(groupdata,function(d) {return d.Day}))
-	.range([0 + padding,width - padding]);
-svg.append("g")
-	.attr("transform", "translate(0," + Theight + ")")
-	.call(d3.axisBottom(xAxis));
-
-// Y axis
-console.log(groupdata)
-var yAxis = d3.scaleLinear()
-	.domain([0,d3.max(groupdata,function(d){return +d.Cases})])
-	.range([height - padding ,0 + padding ])
-svg.append("g")
-	.attr("transform", "translate("+ padding +",0)")
-	.call(d3.axisLeft(yAxis));
-
-svg.append("path")
-	.datum(groupdata)
-	.attr("fill","none")
-	.attr("stroke","steelblue")
-	.attr("stroke-width", 1.5)
-	.attr("id", "data_line")
-	.attr("d", d3.line()
-	  .x(function(d) { return xAxis(d.Day) })
-	  .y(function(d) { return yAxis(d.Cases) })
-	);
-
+//function to create the list on the right with all the information about the state
 var testfunc = function(d,give_data){
 	var day = d.target.__data__.Day
-	//var todo_item = data.filter(c => {return c.date ===day})
 	var test2 = d3.filter(data,function(d){return d.date.getTime() === day.getTime()})
 	if (current != ""){
 		test2 = d3.filter(test2,function(d){return d.continent === current})
@@ -110,23 +73,71 @@ var testfunc = function(d,give_data){
 	const htmlcode = `<li></li><li style="font-weight: 600;"><div class="left">TOTALE</div><div class="right">${give_data.find(d => d.Day === day).Cases}</div></li>` +
 		`<li></li><li style="font-weight: 700;"><div class="left">Paese</div><div class="right">Casi</div></li><li></li>` +
 		test2.map(data => `<li><button class="left_button" id="list_button" onclick="selectplace(this)">${data.country}</button><div class="right">${data.cases}</div></li>`.trimEnd()).join("");;
-	var datefin = [day.getDate(), day.getMonth() + 1, day.getFullYear()].join(' - ')
-	//console.log(test2)
-	//console.log(day)
+	const datefin = [day.getDate(), day.getMonth() + 1, day.getFullYear()].join(' - ')
+	const dateend = [day.getDate() + 7, day.getMonth() + 1, day.getFullYear()].join(' - ')
 	
-	document.getElementById("info").innerHTML = '<ul id:"list">' + `<li style="font-weight: 700;">${datefin}</li>` + htmlcode + '</ul>'
+	document.getElementById("info").innerHTML = '<ul id:"list">' + `<li style="font-weight: 700;"> Dal ${datefin} al ${da}</li>` + htmlcode + '</ul>'
 }
 
-svg.selectAll(".dot")
-	.data(groupdata)
-	.join("circle") // enter append
-		.attr("class", function(d){return "dot" + d.Cases})
-		.attr("r", "3") // radius
-		.attr("cx", d=> xAxis(new Date(d.Day)))   // center x passing through your xScale
-		.attr("cy", d=> yAxis(parseInt(d.Cases)))   // center y through your yScale
-	.on("click",(evento)=>(testfunc(evento,groupdata)))
+// Function to create the graph based on the array that is passed
+// there is no other parameter passed becouse the graph is always the same for the moment 
+function make_graph(element,new_data){
 
+	// X axis
+	var Theight = height - padding
+	var xAxis = d3.scaleTime()
+		.domain(d3.extent(new_data,function(d) {return d.Day}))
+		.range([0 + padding,width - padding]);
+	element.append("g")
+		.attr("transform", "translate(0," + Theight + ")")
+		.call(d3.axisBottom(xAxis));
 
+	// Y axis
+	var yAxis = d3.scaleLinear()
+		.domain([0,d3.max(new_data,function(d){return +d.Cases})])
+		.range([height - padding ,0 + padding ])
+	element.append("g")
+		.attr("transform", "translate("+ padding +",0)")
+		.call(d3.axisLeft(yAxis));
+
+	//rebuild the graph
+	element.append("path")
+		.datum(new_data)
+		.attr("fill","none")
+		.attr("stroke","steelblue")
+		.attr("stroke-width", 1.5)
+		.attr("id", "data_line")
+		.attr("d", d3.line()
+			.x(function(d) { return xAxis(d.Day) })
+			.y(function(d) { return yAxis(d.Cases) })
+		);
+
+	//rebuild the point
+	element.selectAll(".dot")
+		.data(new_data)
+		.join("circle") // enter append
+			.attr("class", function(d){return "dot" + d.Cases})
+			.attr("r", "3") // radius
+			.attr("cx", d=> xAxis(new Date(d.Day)))   // center x passing through your xScale
+			.attr("cy", d=> yAxis(parseInt(d.Cases)))   // center y through your yScale
+		.on("click",(evento)=>(testfunc(evento,new_data)))	
+
+}
+
+// Remake the graph resetting the axis
+function remake_graph(element,new_data){
+
+	// //clear the graph
+	element.selectAll("circle").remove()
+	element.selectAll("#data_line").remove()
+	element.selectAll("g").remove()
+
+	make_graph(element,new_data)
+}
+
+make_graph(svg,groupdata)
+
+//Function that based on the button pressed choose what information to display
 function filter_data(continent){
 	if (current != continent && continent != ""){
 		var data_continent = d3.filter(data,function(d){return d.continent === continent})
@@ -134,107 +145,20 @@ function filter_data(continent){
 
 		groupdata = getArrByWeek(data_continent)
 
-
-		// //clear the graph
-		svg.selectAll("circle").remove()
-		svg.selectAll("#data_line").remove()
-		svg.selectAll("g").remove()
-
-		var Theight = height - padding
-		var xAxis = d3.scaleTime()
-			.domain(d3.extent(groupdata,function(d) {return d.Day}))
-			.range([0 + padding,width - padding]);
-		svg.append("g")
-			.attr("transform", "translate(0," + Theight + ")")
-			.call(d3.axisBottom(xAxis));
-
-		// Y axis
-		console.log(groupdata)
-		var yAxis = d3.scaleLinear()
-			.domain([0,d3.max(groupdata,function(d){return +d.Cases})])
-			.range([height - padding ,0 + padding ])
-		svg.append("g")
-			.attr("transform", "translate("+ padding +",0)")
-			.call(d3.axisLeft(yAxis));
-
-		//rebuild the graph
-		svg.append("path")
-			.datum(groupdata)
-			.attr("fill","none")
-			.attr("stroke","steelblue")
-			.attr("stroke-width", 1.5)
-			.attr("id", "data_line")
-			.attr("d", d3.line()
-				.x(function(d) { return xAxis(d.Day) })
-				.y(function(d) { return yAxis(d.Cases) })
-			);
-
-		svg.selectAll(".dot")
-			.data(groupdata)
-			.join("circle") // enter append
-				.attr("class", function(d){return "dot" + d.Cases})
-				.attr("r", "3") // radius
-				.attr("cx", d=> xAxis(new Date(d.Day)))   // center x passing through your xScale
-				.attr("cy", d=> yAxis(parseInt(d.Cases)))   // center y through your yScale
-			.on("click",(evento)=>(testfunc(evento,groupdata)))
-
+		remake_graph(svg,groupdata)
 	}
 	else if(current != continent && continent == ""){
-		current = continent
-		var groupdata = d3.rollups(
-			data,
-			xs => d3.sum(xs, x => x.cases),
-			d => d.date
-		)
-		.map(([k,v]) => ({Day: k, Cases: v}))
-
-		svg.selectAll("circle").remove()
-		svg.selectAll("#data_line").remove()
-		svg.selectAll("g").remove()
-
-		var Theight = height - padding
-		var xAxis = d3.scaleTime()
-			.domain(d3.extent(groupdata,function(d) {return d.Day}))
-			.range([0 + padding,width - padding]);
-		svg.append("g")
-			.attr("transform", "translate(0," + Theight + ")")
-			.call(d3.axisBottom(xAxis));
-
-		// Y axis
-		console.log(groupdata)
-		var yAxis = d3.scaleLinear()
-			.domain([0,d3.max(groupdata,function(d){return +d.Cases})])
-			.range([height - padding ,0 + padding ])
-		svg.append("g")
-			.attr("transform", "translate("+ padding +",0)")
-			.call(d3.axisLeft(yAxis));
-
-		svg.append("path")
-			.datum(groupdata)
-			.attr("fill","none")
-			.attr("stroke","steelblue")
-			.attr("stroke-width", 1.5)
-			.attr("id", "data_line")
-			.attr("d", d3.line()
-				.x(function(d) { return xAxis(d.Day) })
-				.y(function(d) { return yAxis(d.Cases) })
-			);
-
-		svg.selectAll(".dot")
-			.data(groupdata)
-			.join("circle") // enter append
-				.attr("class", function(d){return "dot" + d.Cases})
-				.attr("r", "3") // radius
-				.attr("cx", d=> xAxis(new Date(d.Day)))   // center x passing through your xScale
-				.attr("cy", d=> yAxis(parseInt(d.Cases)))   // center y through your yScale
-			.on("click",(evento)=>(testfunc(evento,groupdata)))
-
+		groupdata = getArrByWeek(data)
+		
+		remake_graph(svg,groupdata)
+		current=""
 	}
 	else {
 		return 0
 	}
 }
 
+//Get the button pressed
 var buttons = document.getElementsByClassName("select_button");
 for (var i = 0; i < buttons.length; i++) {
 	buttons[i].onclick = function() {
@@ -242,8 +166,8 @@ for (var i = 0; i < buttons.length; i++) {
 	};
 }
 
+//Show a graph in the bottom part of the page with the information by state day by day
 function selectplace(i){
-	//ATTENZIONE, IL VECCHIO GRAFICO NON VIENE RIMOSSO
 	var selected_place = i.outerText
 	console.log(selected_place)
 
@@ -257,41 +181,11 @@ function selectplace(i){
 	)
 	.map(([k,v]) => ({Day: k, Cases: v}))
 
-	// X asix
-	var Theight_2 = height_2 - padding
-	var xAxis_2 = d3.scaleTime()
-		.domain(d3.extent(groupdata,function(d) {return d.Day}))
-		.range([0 + padding,width_2 - padding]);
-	svg_2.append("g")
-		.attr("transform", "translate(0," + Theight_2 + ")")
-		.call(d3.axisBottom(xAxis_2));
-
-	// Y axis
-	var yAxis_2 = d3.scaleLinear()
-		.domain([0,d3.max(groupdata,function(d){return +d.Cases})])
-		.range([height_2 - padding ,0 + padding ])
-	svg_2.append("g")
-		.attr("transform", "translate("+ padding +",0)")
-		.call(d3.axisLeft(yAxis_2));
-
-	svg_2.append("path")
-		.datum(groupdata)
-		.attr("fill","none")
-		.attr("stroke","steelblue")
-		.attr("stroke-width", 1.5)
-		.attr("id", "data_line")
-		.attr("d", d3.line()
-			.x(function(d) { return xAxis_2(d.Day) })
-			.y(function(d) { return yAxis_2(d.Cases) })
-		);
-
-	svg_2.selectAll(".dot")
-	.data(groupdata)
-	.join("circle") // enter append
-		.attr("class", function(d){return "dot" + d.Cases})
-		.attr("r", "3") // radius
-		.attr("cx", d=> xAxis_2(new Date(d.Day)))   // center x passing through your xScale
-		.attr("cy", d=> yAxis_2(parseInt(d.Cases)))   // center y through your yScale
-	.on("click",testfunc)
-
+	if(isShowing){
+		remake_graph(svg_2,groupdata)
+	}
+	else{
+		make_graph(svg_2,groupdata)
+		isShowing = true
+	}
 }
